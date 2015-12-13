b9bde3d668d1ec2c3137e94a06b5cb551fade202
imalan <alanchen@nyu.edu>
2015-12-12 17:07:52 -0800

    datanode

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from dfs.namenode import Namenode
+from dfs.datanode import Datanode
+nn = Namenode()
+nn.save("user/alan/resume/resume.txt", 40000, 1)
+nn.get("user/alan/resume/resume.txt",1)
 
-nm = Namenode()
-nm.save("user/alan/resume/resume.txt", 40000, 1)
-nm.get("user/alan/resume/resume.txt",1)
+dn = Datanode()
+dn.write('27d5482eebd075de44389774fce28c69f45c8a75', 10101, "hello world")
