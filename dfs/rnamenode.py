0985493c482aeac9408f5278ec12dc275d21218f
Edgar Montano <em2532@nyu.edu>
2015-12-18 11:15:42 -0500

    updated write function in rnamenode

--- dfs/rnamenode.py
+++ dfs/rnamenode.py
@@ -3,48 +3,36 @@
 #################
 
 import time
-import commonlib
-import data_pb2
+import namenode_pb2
 import dfss
 from grpc.beta import implementations
 import sys 
-class DataNode(data_pb2.BetaDataNodeServicer):
+from namenode import Namenode
+
+#global vars
+TIMEOUT = 10
+DEBUG = True
+
+class NameNode(namenode_pb2.BetaDataNodeServicer):
     
     def Store(self,request,context):
-        """Stores a file on the data node, DataNode should make a call
-        to our custom DFSS. Therefore it makes a call to dfss.Store()
-        after the proper parameters are passed.
-
+        """
         StoreRequest takes the following parameters:
-            1. string file_name
-            2. bytes file_content 
+            1. string file_path
+            2. int32 file_size
             3. string timestamp
-            4. string user_id
 
         StoreReply takes the following parameters:
-            1. string reply_msg
-            2. bool success
+            1. string path
+            2. string datanodes
+            3. int32 block_size
+            4. bool success
         """
+        nn = Namenode()
+        data_nodes = nn.save(request.file_path,request.file_size,request.timestamp)
          
-        if commonlib.DEBUG:
-            print("Attempting to access DataNode.Store") 
-        
-        #test if the request parameters are properly passed, complain if
-        #nonetype
-        try:
-            fn = request.file_name
-            fc = request.file_content
-            ts = request.timestamp
-            uid = request.user_id
-        except:
-            print("Error in retrieving requests")
-            return data_pb2.StoreReply(reply_msg="Err -2",success=False)
-        
-        #NOTE: Adjust parameters for dfss store operation 
-        dfss.Store(fn,fc,ts,uid)
-        reply_msg = "File successfully written"
 
-        return data_pb2.StoreReply(reply_msg=request.file_name,success=True)
+        return namenode_pb2.StoreReply(path=request.file_path,datanodes=data_nodes,nn.blocksize,success=True)
 
     def Read(self,request,context):
         """Reads a file from data node. This should simply call
@@ -91,7 +79,7 @@ def main():
     port = sys.argv[1]
 
     print("\n\tStarting server on localhost:"+port)
-    server = data_pb2.beta_create_DataNode_server(DataNode())
+    server = data_pb2.beta_create_NameNode_server(NameNode())
     ip = "[::]:"+str(port)
     server.add_insecure_port(ip)
     server.start()
