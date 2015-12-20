5259bb3e10e72846bee547e887397d9bb135c5fe
imalan <alanchen@nyu.edu>
2015-12-19 22:14:10 -0800

    implemented shadow filesystem

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from dfs.datanode import Datanode
 nn = Namenode()
 nn.save("user/alan/resume/resume.txt", 40000, 1)
 nn.get("user/alan/resume/resume.txt",1)
-print nn.blocksize
-dn = Datanode()
-dn.write('27d5482eebd075de44389774fce28c69f45c8a75', 10101, "hello world")
-dn.read('27d5482eebd075de44389774fce28c69f45c8a75', 10101)
+#print nn.blocksize
+#dn = Datanode()
+#dn.write('27d5482eebd075de44389774fce28c69f45c8a75', 10101, "hello world")
+#dn.read('27d5482eebd075de44389774fce28c69f45c8a75', 10101)
